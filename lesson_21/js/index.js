b8cdeb22a45e37e0235ae76c476131730b0488ba
ilyaposlayko <ilyaposlayko@gmail.com>
2022-05-28 06:05:35 +0300

    test commits on github

--- lesson_21/js/index.js
+++ lesson_21/js/index.js
@@ -9,6 +9,7 @@ showNextBtn.addEventListener('click', onShowNextBtnClick);
 
 //create images array
 var imagesUrls = [];
+imagesUrls.push('https://klike.net/uploads/posts/2019-05/1559021877_4.jpg');
 imagesUrls.push('https://klike.net/uploads/posts/2019-05/1559021799_1.jpg');
 imagesUrls.push('https://klike.net/uploads/posts/2019-05/medium/1559021804_2.jpg');
 imagesUrls.push('https://klike.net/uploads/posts/2019-05/1559021828_3.jpg');
